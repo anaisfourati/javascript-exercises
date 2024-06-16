22225c7c7ddcae2509c9b69b7f2dd30f8d9bba95
anaisfourati <103164186+anaisfourati@users.noreply.github.com>
2024-06-16 21:08:45 +0200

    Create a reversed string function.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let finalString = "";
+    const stringArray = string.split("");
+    stringArray.reverse();
+    finalString = stringArray.join("");
+    return finalString;
 };
+
+console.log(reverseString("Anaïs"));
 
 // Do not edit below this line
 module.exports = reverseString;
